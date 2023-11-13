48a182bb0fdc113a50ce4c55ce3d99b5122d9d84
Abolfazl Adhami <aadhami880@gmail.com>
2023-11-13 23:56:27 +0330

    billboard:hook is completed

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useCallback } from "react";
+import useBillboard from "@/hooks/useBillboard";
+import useInfoModalStore from "@/hooks/useInfoModalStore";
 
-function Billboard() {
-  return <div></div>;
-}
+import PlayButoon from "@/components/PlayButton";
+import { InformationCircleIcon } from "@heroicons/react/24/solid";
+
+const Billboard: React.FC = () => {
+  const { data } = useBillboard();
+  const { openModal } = useInfoModalStore();
+
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data]);
+
+  return <div className="relative h-[56.25vw]"></div>;
+};
 
 export default Billboard;
